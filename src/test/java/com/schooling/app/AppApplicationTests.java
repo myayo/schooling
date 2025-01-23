9bae55ff44b38094b57d64b22f38257e7c70f9f0
mdn <marcel.djongon@sciforma.com>
2025-01-23 12:25:38 +0100

    Schooling-1: init spring project

---
+++ src/test/java/com/schooling/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schooling.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
